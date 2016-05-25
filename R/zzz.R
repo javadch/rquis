f1a7ab1a28daf3c2107fc39169d0f279e7b86ca7
Javad Chamanara <chamanara@gmail.com>
2016-05-25 11:31:09 +0200

    unset JAVA_HOME in the R sessions, while working with rJava

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,6 @@
 .onLoad <- function (libname, pkgname) {
   options(java.parameters = "-Xmx8000m") # set the max Java heap size in MB
+  if (Sys.getenv("JAVA_HOME")!="") # Unsetting JAV_HOME for current R session, as requested by rJava package
+    Sys.setenv(JAVA_HOME="")
   .jpackage(pkgname, jars = '*', lib.loc = libname)
 }
